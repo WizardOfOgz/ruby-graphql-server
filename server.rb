f0eff6afcc8e8eb04081d073e3058a88cfcd9984
Andy Ogzewalla <andy.ogzewalla@cybergrx.com>
2018-07-27 09:19:20 -0600

    Return JSON response

--- server.rb
+++ server.rb
@@ -1,9 +1,14 @@
 require "socket"
+require "json"
 
 port = ENV["PORT"] || 4444
 server = TCPServer.new(port)
 
 puts "Listening on port #{ port }..."
+
+data = {
+  users: %w[Bob Fred Mary]
+}
 
 # Loop to handle incoming requests sequentially.
 while session = server.accept
@@ -11,10 +16,11 @@ while session = server.accept
 
   puts request # Log the incoming request
 
+  # Response
   session.print "HTTP/1.1 200\r\n"
-  session.print "Content-Type: text/html\r\n"
+  session.print "Content-Type: application/json\r\n"
   session.print "\r\n"
-  session.print "OK\r\n"
+  session.print data.to_json
 
   session.close
 end
