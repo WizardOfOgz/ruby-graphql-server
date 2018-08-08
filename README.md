5e4f26d03f79f70314d8f6ec6edb1cd6f2fd40ff
Andy Ogzewalla <andy.ogzewalla@cybergrx.com>
2018-08-08 01:21:08 -0500

    GraphQL implementation
    Parse request headers.
    Read POST body.
    Parse JSON params.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby GraphQL Server
+
+This is a demonstration of how to create a basic GraphQL server in Ruby.
+
+Clone the repository and then run `ruby server.rb`.
--- server.rb
+++ server.rb
@@ -1,26 +1,57 @@
+$:.unshift File.dirname(__FILE__)
+require 'bundler/setup'
 require "socket"
 require "json"
+require "graphql"
+require "schema"
+
+def parse_headers(session)
+  # Thanks StackOverflow
+  {}.tap do |headers|
+    while line = session.gets                           # Collect HTTP headers
+      line = line.split(' ', 2)
+      break if line[0] == ""                            # Blank line means no more headers
+      headers[line[0].chop] = line[1].strip             # Hash headers by type
+    end
+  end
+end
+
+
+### Start server
 
 port = ENV["PORT"] || 4444
-server = TCPServer.new(port)
+socket = TCPServer.new(port)
 
 puts "Listening on port #{ port }..."
 
-data = {
-  users: %w[Bob Fred Mary]
-}
-
 # Loop to handle incoming requests sequentially.
-while session = server.accept
+while session = socket.accept
   request = session.gets
+  puts "\n#{ request }"  # Log the incoming request
 
-  puts request # Log the incoming request
+  headers = parse_headers(session)
+  puts "Headers:\n#{ headers.map{|k,v| "#{ k }: #{ v }"  }.join("\n") }"
+
+  # Read the request body
+  content_length = headers["Content-Length"].to_i
+  body = session.read(content_length)
+
+  # We only accept JSON
+  params = JSON.parse(body)
+
+  # Run the query
+  query = params["query"]
+  puts "Processing GraphQL document:\n#{ query }"
+  result = Schema.execute(query)
+
+  puts result.to_json
 
   # Response
-  session.print "HTTP/1.1 200\r\n"
+  session.print "HTTP/1.1 200 OK\r\n"
   session.print "Content-Type: application/json\r\n"
   session.print "\r\n"
-  session.print data.to_json
+  session.print result.to_json
+  puts "\n=> Completed 200 OK"
 
   session.close
 end
