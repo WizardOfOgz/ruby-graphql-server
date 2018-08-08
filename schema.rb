935c7b8cad1429ac1f32905d781c03a64c52827e
Andy Ogzewalla <andy.ogzewalla@cybergrx.com>
2018-08-08 01:10:14 -0500

    GraphQL implementation
    Parse request headers.
    Read POST body.
    Parse JSON params.

---
+++ schema.rb
@@ -0,0 +1,48 @@
+require "graphql"
+
+### Our target—allow clients to query a test
+#
+# query {
+#   test              <- Need to define a "test" field
+# }
+# => "Hello, world!"  <- Need to send a test response
+
+# Actually, a GraphQL response will have a top level key called "data", under
+# which we can find the data we requested. Notice that the JSON structure
+# matches that of the query document.
+# => {
+#      "data": {
+#        "test": "Hello, world!"
+#      }
+#    }
+
+
+### Define Schema
+# We could parse the GraphQL document ourselves, but why do that when there
+# are perfectly good libraries which already exist? Here we define a GraphQL
+# schema using the graphql gem.
+
+# Root-level fields
+class Query < GraphQL::Schema::Object
+  # Add the "test" field with its type specification
+  field :test, String, null: false, description: "Just testing things out."
+
+  # Define a resolver for the "test" field
+  def test(*args)
+    "Hello, world!"
+  end
+
+  field :foo, String, null: false, description: "-bar? -fighters? -manchu?"
+
+  # Some extra stuff just for fun
+  def foo(*args)
+    "Goodbye, cruel world! 😢"
+  end
+end
+
+# Define the schema
+class Schema < GraphQL::Schema
+  # Add the root-level query object.
+  query(Query)
+  # This is also the place where will add mutations and subscriptions later.
+end
--- server.rb
+++ server.rb
@@ -1,26 +1,73 @@
+$:.unshift File.dirname(__FILE__)
+require 'bundler/setup'
 require "socket"
 require "json"
+require "graphql"
+require "schema"
+
+def parse_headers(session)
+  # Thanks StackOverflow
+  {}.tap do |headers|
+    while line = session.gets                           # Collect HTTP headers
+      line = line.split(' ', 2)
+      break if line[0] == ""                            # Blank line means no more headers
+      headers[line[0].chop] = line[1].strip             # Hash headers by type
+    end
+  end
+end
+
+
+### Start server
 
 port = ENV["PORT"] || 4444
-server = TCPServer.new(port)
+socket = TCPServer.new(port)
 
 puts "Listening on port #{ port }..."
 
-data = {
-  users: %w[Bob Fred Mary]
-}
-
 # Loop to handle incoming requests sequentially.
-while session = server.accept
+while session = socket.accept
   request = session.gets
+  puts "\n#{ request.inspect }"  # Log the incoming request
 
-  puts request # Log the incoming request
+  headers = parse_headers(session)
+  puts "Headers:\n#{ headers.map{|k,v| "#{ k }: #{ v }"  }.join("\n") }"
+
+  # The body is the GraphQL document
+  content_length = headers["Content-Length"].to_i
+  body = session.read(content_length)
+
+  # We only accept JSON
+  begin
+    params = JSON.parse(body)
+  rescue JSON::ParserError
+    session.print "HTTP/1.1 400 You didn't do your JSON right\r\n"
+    session.close
+    puts "\n=> Completed 400 Bad JSON"
+    next
+  end
+
+  query = params["query"]
+
+  unless query
+    session.print "HTTP/1.1 400 You forgot to send a query\r\n"
+    session.close
+    puts "\n=> Completed 400 Missing Query"
+    next
+  end
+
+  # HERE'S THE GOOD STUFF!!!
+  # Run the requested query
+  puts "Processing GraphQL document:\n#{ query }"
+  result = Schema.execute(query)
+
+  puts result.to_json
 
   # Response
-  session.print "HTTP/1.1 200\r\n"
+  session.print "HTTP/1.1 200 OK\r\n"
   session.print "Content-Type: application/json\r\n"
   session.print "\r\n"
-  session.print data.to_json
+  session.print result.to_json
+  puts "\n=> Completed 200 OK"
 
   session.close
 end
